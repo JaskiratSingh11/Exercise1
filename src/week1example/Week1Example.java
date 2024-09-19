8776faffd9550bf2e6c6bd4bc7f57b8502afab0c
JaskiratSingh11 <singh891@sheridancollege.ca>
2024-09-19 19:16:29 -0400

    Week1Example1

---
+++ src/week1example/Week1Example.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week1example;
+
+/**
+ *
+ * @author jksg1
+ */
+public class Week1Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to Java Week 1");
+    }
+    
+}
